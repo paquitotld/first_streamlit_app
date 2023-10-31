01d50f7fc5c2c1694ace833a0e4355dbc6fc9da6
paquitotld <149540447+paquitotld@users.noreply.github.com>
2023-10-31 23:24:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit
 streamlit.title ('Estoy haciendo un curso')
 streamlit.title ('Probando 1, 2, 3')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text(' 🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🥑🍞 Hard-Boiled Free-Range Egg')
 
 
